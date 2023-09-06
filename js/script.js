593af04bb710c8340f5b946520ea0960d16e053d
Jeong Hoon <gbg9609@naver.com>
2023-09-06 21:00:12 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 let computerNum = 0
 
+//숫자가 똑같을때 기회 깍지 말것 + 이미 입력하셨습니다.
+//숫자가 똑같은걸 먼저 만들어야됨, 그러면 입력한 숫자를 담아둬야지
 let inNum = document.getElementById('user-input')
 let btn = document.getElementById('go-btn')
 let result = document.getElementById('result')
 let reset = document.getElementById('reset')
+let saveChances = document.getElementById('chances')
+let putNum = []
 let chances = 5
 
 
@@ -17,9 +21,21 @@ function ranNum(){
   }
 
 function play(){
-  chances--
-  console.log('기회', chances);
   let userValue = inNum.value
+  if(userValue > 100 || userValue < 1){
+    result.textContent = '1부터 100까지 숫자를 입력해주세요'
+    return;
+  }
+
+  if(putNum.includes(userValue)){
+    result.textContent = '이미 입력한 숫자 입니다. 다른 숫자를 입력해 주세요'
+    return;
+  }
+
+  chances--
+  saveChances.innerHTML = `남은기회 : ${chances}번`
+  console.log('기회', chances);
+  
   if(userValue > computerNum){
     result.innerHTML = 'Down!!'
   }else if(userValue < computerNum){
@@ -28,16 +44,23 @@ function play(){
     result.innerHTML = '맞췄습니다!!'
   }
 
+  putNum.push(userValue)
+  console.log(putNum);
+  }
+
   if(chances < 1){
     btn.disabled = true
   }
-}
+
 
 function resetBtn() {
   ranNum()
   result.innerHTML = ''
   inNum.value = ''
   inNum.focus()
+  btn.disabled = false
+  chances = 5
+  saveChances.textContent = `남은기회 : ${chances}번`
 }
 
 ranNum()
